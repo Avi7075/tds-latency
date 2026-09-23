606e23125f981521b0bdd5e9f00cd6802e6b3248
Avi Sehgal <avisehgal7075@gmail.com>
2026-09-24 01:01:31 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,6 +12,7 @@ async def cors(request: Request, call_next):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Expose-Headers"] = "*"
     return response
 
 # (latency_ms, uptime_pct) per region, from q-vercel-latency.json
